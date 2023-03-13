1019f63b8ace04ab9021c9c5bd3d7914c972b405
cattyngmd <cvenby@gmail.com>
2023-03-13 14:45:33 +0300

    Improved formatting and added .env.tmp

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,7 +3,7 @@ import { client as c } from '../index'
 
 export default new Event('ready', async () => {
     const commands = Array.from(c.commands.values()).map(c => c.data.toJSON())
-    const guildId = process.env.guildId
+    const guildId = process.env.GUILD_ID
 
     if (guildId) await c.guilds.cache.get(guildId).commands.set(commands)
     else await c.application.commands.set(commands)
