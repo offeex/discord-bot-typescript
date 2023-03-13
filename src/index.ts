87ea60742f84cb51115ff98a8e1bf6a46e26853d
offeex <rockezprod@gmail.com>
2023-03-13 15:15:12 +0200

    clean comment

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,3 @@ config()
 
 export const client = new ExtendedClient()
 client.start().then(() => console.log('Started!'))
-
-// const cm = new ContextMenuCommandBuilder()
-// 	.setName('test')
-// 	.setType(ApplicationCommandType.User)
-//
-// client.on('interactionCreate', async interaction => {
-// 	if (!interaction.isUserContextMenuCommand()) return
-//
-// 	const { username } = interaction.targetUser
-// 	console.log(username)
-// 	interaction.reply(`Hello, ${username}!`)
-// })
